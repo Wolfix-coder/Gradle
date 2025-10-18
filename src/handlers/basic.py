a1ef840985e0035eb0eee60d82a67edeae8b9af9
Wolfix_coder <arremkintolp@gmail.com>
2025-10-18 16:33:30 +0300

    Vetsion 1.5.1
    1. Виправлення проблем для хостингу.

--- src/handlers/basic.py
+++ src/handlers/basic.py
@@ -11,24 +11,24 @@ basic_router = Router()
 
 @basic_router.message(F.text.lower().contains("допомога"))
 async def text_help(message: types.Message):
-    await message.answer(help_text, reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(help_text, parse_mode="Markdown", reply_markup=types.ReplyKeyboardRemove())
 
 @basic_router.message(Command("help"))
 async def help_handler(message: types.Message):
-    await message.answer(help_text, reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(help_text, parse_mode="Markdown", reply_markup=types.ReplyKeyboardRemove())
 
 @basic_router.message(Command("help_admin"))
 @require_admin
 async def help_handler(message: types.Message):
-    await message.answer(help_text_admin, reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(help_text_admin, parse_mode="Markdown", reply_markup=types.ReplyKeyboardRemove())
 
 @basic_router.message(Command("about"))
 async def cmd_about(message: types.Message):
-    await message.answer(about_text, reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(about_text, parse_mode="Markdown", reply_markup=types.ReplyKeyboardRemove())
 
 @basic_router.message(Command("price"))
 async def cmd_price(message: types.Message):
-    await message.answer(price_text, reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(price_text, parse_mode="Markdown", reply_markup=types.ReplyKeyboardRemove())
 
 @basic_router.message(Command("support"))
 async def cmd_support(message: types.Message):
